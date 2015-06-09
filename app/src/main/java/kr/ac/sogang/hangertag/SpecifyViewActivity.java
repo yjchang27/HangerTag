a4a3d93789eec95dd9ed3c50901ff466a1399b76
kidsnow <kidsnow@sogang.ac.kr>
2015-06-09 20:01:03 +0900

    Beacon장착

--- app/src/main/java/kr/ac/sogang/hangertag/SpecifyViewActivity.java
+++ app/src/main/java/kr/ac/sogang/hangertag/SpecifyViewActivity.java
@@ -1,6 +1,8 @@
 package kr.ac.sogang.hangertag;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,17 +10,40 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.Region;
+import org.altbeacon.beacon.powersave.BackgroundPowerSaver;
+import org.altbeacon.beacon.startup.RegionBootstrap;
+
 /**
  * Created by Kidsnow on 2015-04-12.
  */
 
 
 public class SpecifyViewActivity extends Activity implements View.OnClickListener{
+    private static final String TAG = "AndroidProximityReferenceApplication";
+    private RegionBootstrap regionBootstrap;
+    private BackgroundPowerSaver backgroundPowerSaver;
+    private boolean haveDetectedBeaconsSinceBoot = false;
+    private SpecifyViewActivity specifyViewActivity = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_specify_view);
 
+        verifyBluetooth();
+
+        ImageButton btGoSpec1 = (ImageButton)findViewById(R.id.specification1);
+        btGoSpec1.setBackgroundResource(R.mipmap.blouson0);
+        ImageButton btGoSpec2 = (ImageButton)findViewById(R.id.specification2);
+        btGoSpec2.setBackgroundResource(R.mipmap.white);
+        ImageButton btGoSpec3 = (ImageButton)findViewById(R.id.specification3);
+        btGoSpec3.setBackgroundResource(R.mipmap.white);
+        ImageButton btGoSpec4 = (ImageButton)findViewById(R.id.specification4);
+        btGoSpec4.setBackgroundResource(R.mipmap.white);
+        /*
         ImageButton btGoSpec1 = (ImageButton)findViewById(R.id.specification1);
         btGoSpec1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -37,14 +62,14 @@ public class SpecifyViewActivity extends Activity implements View.OnClickListene
         ImageButton btGoSpec2 = (ImageButton)findViewById(R.id.specification2);
         btGoSpec2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(SpecifyViewActivity.this,DetailViewActivity.class);
+                Intent intent = new Intent(SpecifyViewActivity.this, DetailViewActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
                 ItemSet itemSet = new ItemSet();
                 itemSet.description = "2번 상품이다";
                 itemSet.imageList.add(R.mipmap.coat0);
                 itemSet.imageList.add(R.mipmap.coat1);
                 itemSet.imageList.add(R.mipmap.coat2);
-                intent.putExtra("itemSet",itemSet);
+                intent.putExtra("itemSet", itemSet);
                 startActivity(intent);
             }
         });
@@ -52,14 +77,14 @@ public class SpecifyViewActivity extends Activity implements View.OnClickListene
         ImageButton btGoSpec3 = (ImageButton)findViewById(R.id.specification3);
         btGoSpec3.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(SpecifyViewActivity.this,DetailViewActivity.class);
+                Intent intent = new Intent(SpecifyViewActivity.this, DetailViewActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
                 ItemSet itemSet = new ItemSet();
                 itemSet.description = "3번 상품이다";
                 itemSet.imageList.add(R.mipmap.denim0);
                 itemSet.imageList.add(R.mipmap.denim1);
                 itemSet.imageList.add(R.mipmap.denim2);
-                intent.putExtra("itemSet",itemSet);
+                intent.putExtra("itemSet", itemSet);
                 startActivity(intent);
             }
         });
@@ -67,17 +92,17 @@ public class SpecifyViewActivity extends Activity implements View.OnClickListene
         ImageButton btGoSpec4 = (ImageButton)findViewById(R.id.specification4);
         btGoSpec4.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(SpecifyViewActivity.this,DetailViewActivity.class);
+                Intent intent = new Intent(SpecifyViewActivity.this, DetailViewActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
                 ItemSet itemSet = new ItemSet();
                 //itemSet.description = "4번 상품이다";
                 //itemSet.imageList.add(R.mipmap.coat0);
                 //itemSet.imageList.add(R.mipmap.coat1);
                 //itemSet.imageList.add(R.mipmap.coat2);
-                intent.putExtra("itemSet",itemSet);
+                intent.putExtra("itemSet", itemSet);
                 startActivity(intent);
             }
-        });
+        });*/
     }
 
     @Override
@@ -95,6 +120,42 @@ public class SpecifyViewActivity extends Activity implements View.OnClickListene
             case R.id.specification4:
                 Log.i("Fourth", "OK");
                 break;
+        }
+    }
+
+    private void verifyBluetooth() {
+        try {
+            if (!BeaconManager.getInstanceForApplication(this).checkAvailability()) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Bluetooth not enabled");
+                builder.setMessage("Please enable bluetooth in settings and restart this application.");
+                builder.setPositiveButton(android.R.string.ok, null);
+                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        finish();
+                        System.exit(0);
+                    }
+                });
+                builder.show();
+            }
+        }
+        catch (RuntimeException e) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Bluetooth LE not available");
+            builder.setMessage("Sorry, this device does not support Bluetooth LE.");
+            builder.setPositiveButton(android.R.string.ok, null);
+            builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+                @Override
+                public void onDismiss(DialogInterface dialog) {
+                    finish();
+                    System.exit(0);
+                }
+
+            });
+            builder.show();
+
         }
     }
 }
